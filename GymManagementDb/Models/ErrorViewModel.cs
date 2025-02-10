9ed83bcc335292746e4e89869489d6581af5cfea
Feng Feng Chen <ac123069@avcol.school.nz>
2025-02-11 10:54:45 +1300

    Add project files.

---
+++ GymManagementDb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GymManagementDb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
