fe2028e8247f81596b8594047ecfd481ee057799
ac123069 <ac123069@8CG8163L1Y>
2025-02-25 11:11:08 +1300

    I created my members table

---
+++ GymManagementDb/Models/Member.cs
@@ -0,0 +1,13 @@
+﻿namespace GymManagementDb.Models
+{
+    public class Member
+    {
+        public int MemberID { get; set; }
+
+        public string First_Name { get; set; }
+        
+        public string Last_Name { get; set; }
+
+        public 
+    }
+}
